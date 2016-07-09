10d1b5461c7c2f7301e6535d1d499023be545a1b
David De <daviddecoding@gmail.com>
2016-07-09 15:04:15 -0400

    Added schema

--- spark/src/main/scala/com/dde/Main.scala
+++ spark/src/main/scala/com/dde/Main.scala
@@ -4,6 +4,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{StructType, StructField, StringType, DoubleType};
 
 object Main
 {
@@ -11,16 +12,24 @@ object Main
 		val conf = new SparkConf().setMaster("local[4]").setAppName("Playing with Fire")
 		val sc = new SparkContext(conf)
 		val sql = new SQLContext(sc)
+		val schema = StructType(Array(
+				StructField("date", StringType, true),
+				StructField("AverageTemperature", DoubleType, true),
+				StructField("AverageTemperatureUncertainty", StringType, true),
+				StructField("City", StringType, true),
+				StructField("Country", StringType, true),
+				StructField("Latitude", StringType, true),
+				StructField("Longitutde", StringType, true)))
 		val df = sql.read
 			.format("com.databricks.spark.csv")
 			.option("header", "true")
-			.option("inferSchema", "true")
+			.schema(schema)
 			.load("/home/davidde/GlobalLandTemperaturesByCity.csv")
 
 		val dfByCity = df.select("City", "Country", "AverageTemperature").groupBy("City", "Country").agg(max("AverageTemperature"))
 		dfByCity.write
 			.format("com.databricks.spark.csv")
-			.option("header", "true")
+			.option("header", "false")
 			.save("/home/davidde/spark-output")
 	}
 }
